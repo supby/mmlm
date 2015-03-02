b4f7a637de82d988854cd78e5a6bf0a5e189b95f
Andrej <supius@gmail.com>
2015-03-02 22:50:05 +0300

    add minmax scaler for data

--- src/lda.py
+++ src/lda.py
@@ -7,23 +7,30 @@
 __author__ = "andrej"
 __date__ = "$Feb 15, 2015 9:27:13 PM$"
 
-import pickle
+import cPickle as pickle
+import numpy
 from sklearn import metrics
 from sklearn import cross_validation
 from sklearn.lda import LDA
 from sklearn.qda import QDA
+from sklearn import preprocessing
 
 if __name__ == "__main__":
     print 'Load train dataset.'
     
-    dataset = pickle.load(open("../localdata/train_data.pkl", "rb"))
+    dataset = pickle.load(open("../localdata/train_data_0.pkl", "rb"))
 
     train_data = dataset[0]
     train_target = dataset[1]
 
+    print 'Scaling the data.'
+    min_max_scaler = preprocessing.MinMaxScaler()
+    train_data = numpy.mat(min_max_scaler.fit_transform(train_data), 
+                           numpy.float32)
+                           
     X_train, X_test, y_train, y_test = \
         cross_validation.train_test_split(train_data, train_target, 
-                                          test_size=0.3, random_state=0)
+                                          test_size=0.3, random_state=42)
                                           
 #    clf = LDA()
     clf = QDA()
